76012f15913a7ee2cc2d6c8db5fa3fdc708ecacf
Abel Albarracin Garcia <abel.albarracin@hotmail.com>
2016-04-11 01:06:43 +0200

    Albaran

--- shadow/frAlbaran.vb
+++ shadow/frAlbaran.vb
@@ -46,6 +46,10 @@ Public Class frAlbaran
         Me.ReportViewer2.RefreshReport()
         Me.ReportViewer1.RefreshReport()
         Me.ReportViewer2.RefreshReport()
+        Me.ReportViewer1.RefreshReport()
+        Me.ReportViewer2.RefreshReport()
+        Me.ReportViewer1.RefreshReport()
+        Me.ReportViewer2.RefreshReport()
     End Sub
     Public Sub deshabilitarBotones()
         cmdGuardar.Enabled = False
